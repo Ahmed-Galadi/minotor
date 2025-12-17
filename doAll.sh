5cea94ccb18b3f54d586aba3b2d493aaa195f161
Ahmed-Galadi <galadiahmed99@gmail.com>
2025-12-17 07:15:41 +0000

    cleaning the code from previouse tests and binary files

--- doAll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CPU_PERSENTAGE=$(./CPU_CALC)
-RAW_CPU_TEMP=$(< /sys/class/thermal/thermal_zone1/temp)
-CPU_TEMPERTURE=$(echo "scale=1; $RAW_CPU_TEMP / 1000" | bc)
-RAMTOTAL=$(awk '/MemTotal/ { printf "%.1f", $2 / 1024 / 1024 }' /proc/meminfo)
-RAMAVAILABLE=$(awk '/MemAvailable/ { printf "%.1f", $2 / 1024 / 1024 }' /proc/meminfo)
-
